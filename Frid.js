7171bb9a83cfcdd6aa160362bf6f228eea3235c0
ananyeli24 <ananyelibatista@AdminisatorsMBP.lan>
2022-04-22 13:59:58 -0400

    Frid.js

---
+++ Frid.js
@@ -0,0 +1,13 @@
+var myFirstArray = ["Hello", "Hey", "Hi", "Bonjour","Keloke"];
+var mySecondArray = [8,4,24,2,6];
+var myThirdArray = [];
+var myFourthArray = [11,"Sugar",99,"Coffee", 88];
+
+for ( var i = 0; i < myFourthArray; i++) {
+    myThirdArray.push(false)
+}
+
+function myFirst() 
+    myFirst.push("hellow"); {
+}
+console.log(myFirst());
